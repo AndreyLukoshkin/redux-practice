daff495a18d4a2535f8a13c4f2c5102d1cf7ff58
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2023-06-30 03:14:15 +0300

    infiniteScroll, loader from react-loader-spinner Bars

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,39 +1,65 @@
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Bars } from "react-loader-spinner";
+import { useDispatch, useSelector } from "react-redux";
+
 import DetailedCard from "../../components/DetailedCard";
 import Layout from "../../components/Layout";
+import { getPhotos } from "../../redux/actions/photos";
+import "./styles.css";
 
 const MainPage = () => {
+  const photos = useSelector((state) => state.photos.photos);
+  const loading = useSelector((state) => state.photos.isPhotoLoading);
+  const total = useSelector((state) => state.photos.totalPhotos);
+  const dispatch = useDispatch();
+
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    dispatch(getPhotos(page));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const nextHandler = () => {
+    setPage(page + 1);
+  };
+
   return (
     <Layout nickName="Andrii" id={1}>
-      <div>main page</div>
-      <DetailedCard
-        userName="Andrii"
-        userId={1}
-        imgUrl="https://imgix.ranker.com/list_img_v2/3323/2523323/original/ancient-pict-facts-u1"
-        likes={10}
-        isLikedByYou={true}
-        comments={[
-          {
-            text: "asddasd",
-            nickName: "aaadd",
-          },
-          {
-            text: "asddasd",
-            nickName: "aaadd",
-          },
-          {
-            text: "asddasd",
-            nickName: "aaadd",
-          },
-          {
-            text: "asddasd",
-            nickName: "aaadd",
-          },
-          {
-            text: "asddasd",
-            nickName: "aaadd",
-          },
-        ]}
-      />
+      <div className="cnMainPageRoot">
+        {loading ? (
+          <div className="cnMainLoaderContainer">
+            <Bars color="#000BFF" height={80} width={80} />
+          </div>
+        ) : (
+          <InfiniteScroll
+            dataLength={photos.length}
+            next={nextHandler}
+            hasMore={photos.length < total}
+            loader={
+              <div className="cnMainLoaderContainer">
+                <Bars color="#000BFF" height={15} width={15} />
+              </div>
+            }
+            endMessage={<p className="cnMainLoaderContainer">That's all</p>}
+          >
+            {photos.map(({ author, imgUrl, id, likes, comments }) => (
+              <DetailedCard
+                key={id}
+                userName={author.nickname}
+                userId={author.id}
+                avatarUrl={author.avatarUrl}
+                imgUrl={imgUrl}
+                likes={likes.length}
+                isLikedByYou={true}
+                comments={comments}
+                className="cnMainPageCard"
+              />
+            ))}
+          </InfiniteScroll>
+        )}
+      </div>
     </Layout>
   );
 };
--- src/redux/actions/photos.js
+++ src/redux/actions/photos.js
@@ -3,16 +3,27 @@ import {
   getPhotosFailed,
   getPhotosStarted,
   getPhotosSuccess,
+  setPhotosTotal,
 } from "../actionCreators/photos";
 
-export const getPhotos = () => {
-  return async (dispatch) => {
+export const getPhotos = (page = 1) => {
+  return async (dispatch, getState) => {
     try {
-      dispatch(getPhotosStarted);
-      const response = api.photos.getPhotos({
-        params: { _page: 0, _limit: 5 },
+      const store = getState();
+      if (page === 1) {
+        dispatch(getPhotosStarted);
+      }
+
+      const response = await api.photos.getPhotos({
+        params: { _page: page, _limit: 5 },
       });
-      dispatch(getPhotosSuccess(response.data));
+
+      if (page === 1) {
+        dispatch(setPhotosTotal(response.headers["x-total-count"]));
+        dispatch(getPhotosSuccess([...response.data]));
+      } else {
+        dispatch(getPhotosSuccess([...store.photos.photos, ...response.data]));
+      }
     } catch (error) {
       dispatch(getPhotosFailed(error));
     }
